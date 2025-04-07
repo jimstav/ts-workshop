d9cd5608f67e1fe304cedefff037bce99459b3d2
jimstav <jimstav@gmail.com>
2025-04-07 18:09:32 +0300

    basic types exercise implementation

--- src/basic-types/src/initial.ts
+++ src/basic-types/src/initial.ts
@@ -1,19 +1,30 @@
 //use this enum
 enum QuestionCategory {
   GeneralKnowledge = 'GeneralKnowledge',
-  //... add some more
+  Math = 'Math',
+  Science = 'Science',
+  Programming = 'Programming'
 }
 
 //create a type Question
+type Question = {
+  category: QuestionCategory;
+  questionText: string;
+  correctAnswer: boolean;
+}
 
 //create the array of questions. Make it type safe
-const questions = [
-  //..
+const questions: Question[] = [
+  {
+    category: QuestionCategory.GeneralKnowledge,
+    questionText: 'Is the answer to the universe and everything 42?',
+    correctAnswer: true
+  }
 ];
 
 //use the checkAnswer function. Make it type safe
-function checkAnswer(question, userAnswer) {
+function checkAnswer(question: Question, userAnswer: boolean): boolean {
   return question.correctAnswer === userAnswer;
 }
 
-checkAnswer(questions[0], false);
+console.log(checkAnswer(questions[0], false));
